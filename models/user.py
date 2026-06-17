722e06ab8229d3bd628907280927ae9d4bb7735b
David <3dgiordano@gmail.com>
2026-06-17 07:40:29 -0300

    More hints provided

--- models/user.py
+++ models/user.py
@@ -6,3 +6,5 @@ class User(BaseModel):
     display_name: str = Field(description="Display name of the user")
     first_name: str = Field(description="First name of the user")
     last_name: str = Field(description="Last name of the user")
+    cloud_name: str = Field(description="Perfecto cloud name from MCP configuration (PERFECTO_CLOUD_NAME)")
+    cloud_url: str = Field(description="Perfecto cloud portal URL (https://{cloud_name}.app.perfectomobile.com)")
